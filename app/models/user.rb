417bf60f7c339a7907c0ae131e531ed50c8e923f
rafik mrabet <mrabetr@hotmail.com>
2021-04-05 18:31:57 +0100

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable
+
+  def generate_jwt
+    JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+  end
+end
